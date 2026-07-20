a82c41d54b69fae1dc5076cb289a5bec5bdf292a
mathiasmihalceadev <mathias.mihalcea@gmail.com>
2026-07-21 00:33:32 +0300

    Revert "Changes" Yep This This reverts commit a06246a1c0fe049bcf80c229d6ad09fa52c965d9.

--- src/api/list-property/controllers/list-property.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * list-property controller
- */
-
-const { createCoreController } = require('@strapi/strapi').factories;
-
-module.exports = createCoreController('api::list-property.list-property');
--- src/api/list-property/routes/list-property.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * list-property router
- */
-
-const { createCoreRouter } = require('@strapi/strapi').factories;
-
-module.exports = createCoreRouter('api::list-property.list-property');

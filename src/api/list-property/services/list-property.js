d6bec9cdc32a412ced7b43e7a87aae78bf632ca5
mathiasmihalceadev <mathias.mihalcea@gmail.com>
2026-07-21 16:12:47 +0300

    Added property lisiting back

---
+++ src/api/list-property/services/list-property.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * list-property service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::list-property.list-property');
